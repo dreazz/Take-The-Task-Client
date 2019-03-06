2408a1633eaadf8415acc74ec2c2a21d73221ad5
dreazz <agustinsilveira.96@gmail.com>
2019-03-06 16:49:09 +0100

    stats add

--- src/components/TaskContainer.js
+++ src/components/TaskContainer.js
@@ -3,6 +3,7 @@ import TaskCard from './TaskCard'
 import Form from './Form';
 import axios from 'axios';
 import '../static/taskcontainer.css'
+import Swiper from 'react-id-swiper';
 
 import { Bubble, Doughnut, Bar, crazyLine, HorizontalBar, Radar } from 'react-chartjs-2';
 
@@ -11,6 +12,11 @@ export default class TaskContainer extends Component {
 
   state = {
     tasks: [],
+    tasksDone: [],
+    tasksTodo: [],
+    tasksDonelength: 0,
+    tasksTodolength: 0,
+
     task: {
       title: '',
       body: '',
@@ -32,8 +38,17 @@ export default class TaskContainer extends Component {
       .then((tasks) => {
         console.log(tasks.data)
         this.setState({
-          tasks: tasks.data
+          tasks: tasks.data,
+          tasksDone: tasks.data.filter((task) => task.status === 'done'),
+          tasksTodo: tasks.data.filter((task) => task.status === 'todo'),
+         // tasksDoing:tasks.data.filter((task) => task.status === 'doing'),
+
         })
+        this.setState({
+          tasksDonelength: this.state.tasksDone.length,
+          tasksTodolength: this.state.tasksTodo.length,
+        })
+        console.log(this.state.tasksTodolength)
 
       })
   }
@@ -161,62 +176,68 @@ export default class TaskContainer extends Component {
     this.componentDidMount()
   }
 
-  data = {
-    labels: [
-      "hola",
-      'Green',
-      'Yellow'
-    ],
-    datasets: [{
-      data: [300, 50, 100],
-      backgroundColor: [
-        '#FF6384',
-        '#36A2EB',
-        '#FFCE56'
-      ],
-      hoverBackgroundColor: [
-        '#FF6384',
-        '#36A2EB',
-        '#FFCE56'
-      ]
-    }]
-  }
+
 
   render() {
-    return (
-      <div className="task-container">
-        <button className="btn btn-create" onClick={this.showForm}><h3>New Task</h3></button>
-        <button className="btn btn-show-done" onClick={this.showDoneContainer}>{this.state.todoContainer ? <h3>Show done list </h3> : <h3>Show To-Do list</h3>} </button>
+    const data = {
+      labels: ['Todo', 'Done','Total'],
+      datasets: [
+        {
 
-        {(this.state.todoContainer) ?
-          <div className="container">
-            <h3>To-Do</h3>
-            <hr className="container-separator"></hr>
-            <div className="tasks tasks-todo">
-
-              {this.state.tasks.map((task) => {
-                return task.status === 'done' ? false : <TaskCard title={task.title} body={task.body} onClickDelete={() => this.deleteTask(task._id)} showEditForm={() => this.showEditForm(task._id)} done={() => this.done(task._id, task.status)} id={task._id}></TaskCard>
-              })}
-
-            </div>
-            <hr className="container-separator" />
-          </div>
-          :
-          <div className="container">
-            <h3>Done</h3>
-            <hr className="container-separator" />
-            <div className="tasks tasks-done">
-
-              {this.state.tasks.map((task) => {
-                return task.status === 'todo' ? false : <TaskCard title={task.title} body={task.body} id={task._id} done={() => this.done(task._id, task.status)} onClickDelete={() => this.deleteTask(task._id)}></TaskCard>
-              })}
-
-            </div>
-            <hr className="container-separator" />
-          </div>
-
+          backgroundColor: 'rgba(255,99,132,0.2)',
+          borderColor: 'rgba(255,99,132,1)',
+          borderWidth: 1,
+         
+          hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+          hoverBorderColor: 'rgba(255,99,132,1)',
+          data: [this.state.tasksTodolength, this.state.tasksDonelength,this.state.tasks.length,0]
         }
 
+      ]
+    };
+    return (
+      <div className="task-container">
+        <div className="button-container">
+          <button className="btn main-btn btn-create" onClick={this.showForm}><h3>New Task</h3></button>
+          <button className="btn main-btn btn-show-done" onClick={this.showDoneContainer}>{this.state.todoContainer ? <h3>Show done list </h3> : <h3>Show Stats</h3>} </button>
+        </div>
+        {this.state.todoContainer ?
+          <div>
+            <div className="container">
+              <h3>To-Do</h3>
+              <hr className="container-separator"></hr>
+
+              <div className="tasks tasks-todo">
+
+                {this.state.tasks.map((task) => {
+
+                  return task.status === 'done' ? false : <TaskCard title={task.title} body={task.body} onClickDelete={() => this.deleteTask(task._id)} showEditForm={() => this.showEditForm(task._id)} done={() => this.done(task._id, task.status)} id={task._id}></TaskCard>
+                })}
+              </div>
+
+
+            </div>
+
+            <div className="container">
+              <h3>Done</h3>
+              <hr className="container-separator" />
+              <div className="tasks tasks-done">
+
+                {this.state.tasks.map((task) => {
+                  return task.status === 'todo' ? false : <TaskCard title={task.title} body={task.body} id={task._id} done={() => this.done(task._id, task.status)} onClickDelete={() => this.deleteTask(task._id)}></TaskCard>
+                })}
+
+              </div>
+
+            </div>
+          </div>
+          :
+          <div>
+            <HorizontalBar data={data} width={100} height={1000} options={{
+              maintainAspectRatio: false
+            }}></HorizontalBar>
+          </div>
+        }
         {this.state.edit ? <Form onClick={this.showEditForm} onSubmitForm={this.editTask} edit={this.state.edit} onChangeTitle={this.onChangeTitle} onChangeBody={this.onChangeBody}>Edit</Form> : false}
         {this.state.create ? <Form onSubmitForm={this.onSubmitForm} onClick={this.showForm} onChangeTitle={this.onChangeTitle} onChangeBody={this.onChangeBody}>Create</Form> : false}
 
